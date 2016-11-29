0bf6b1f875a571a037585e34a8657de46f7a4e22
haossut <haoss_ut@mail.ru>
2016-11-29 18:01:47 +0200

    no message

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -129,7 +129,7 @@ $(document).ready(function(){
 
   // Sumoselect
   $('.sumoselect').SumoSelect();
-
+  
   // simpleForm version 2015-09-23 14:30 GMT +2
   simpleForm('form.form-callback');
 });
