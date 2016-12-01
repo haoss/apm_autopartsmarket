ce1b7a0144dcf8181e7df81ba53069c08fea2619
haossut <haoss_ut@mail.ru>
2016-12-01 17:40:34 +0200

    no message

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -129,7 +129,12 @@ $(document).ready(function(){
 
   // Sumoselect
   $('.sumoselect').SumoSelect();
-  
+
+  // Nanoscroller
+  $(".nano").nanoScroller({
+    alwaysVisible: true
+  });
+
   // simpleForm version 2015-09-23 14:30 GMT +2
   simpleForm('form.form-callback');
 });
