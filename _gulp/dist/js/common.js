d10731637248132924e0a3e67a10cca21f38da1d
haossut <haoss_ut@mail.ru>
2017-01-17 15:38:00 +0200

    no message

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -193,6 +193,18 @@ $(document).ready(function(){
     });
   });
 
+  // .statistic
+  $('.statistic__header').on('click', function(){
+    $(this).parent().toggleClass('is-open');
+
+    if ($(this).parent().hasClass('is-open')) {
+      $(this).next('.statistic__body').slideDown();
+    } else {
+      $(this).next('.statistic__body').slideUp();
+    }
+
+  });
+
   // Datepicker
   $('#input-datepicker').data('datepicker')
 
@@ -285,3 +297,38 @@ function simpleForm(form, callback) {
     return false;
   });
 }
+
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+'use strict';
+
+;( function ( document, window, index )
+{
+	var inputs = document.querySelectorAll( '.inputfile' );
+	Array.prototype.forEach.call( inputs, function( input )
+	{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				label.querySelector( 'span' ).innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+
+		// Firefox bug fix
+		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+	});
+}( document, window, 0 ));
